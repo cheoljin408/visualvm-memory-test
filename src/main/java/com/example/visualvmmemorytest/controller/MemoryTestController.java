70437a4248c3aa4a5944b1f87984c24daf373faa
cheoljin408 <kcj409@naver.com>
2022-05-25 08:54:44 +0900

    Update MemoryTestController
    - home 메소드 구현
    - Thread.sleep으로 시간 지연

--- src/main/java/com/example/visualvmmemorytest/controller/MemoryTestController.java
+++ src/main/java/com/example/visualvmmemorytest/controller/MemoryTestController.java
@@ -1,13 +1,18 @@
 package com.example.visualvmmemorytest.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@Controller
 public class MemoryTestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
 
     @GetMapping("/memorytest")
     public Integer memoryTest() {
@@ -21,7 +26,7 @@ public class MemoryTestController {
             Runtime rt = Runtime.getRuntime();
             System.out.printf("[%d] free memory: %s%n", index++, rt.freeMemory());
             try {
-                Thread.sleep(10);
+                Thread.sleep(30);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
